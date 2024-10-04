36ecfdef01c3affe596d1a3730f1cd7648e1e15c
Astro <danielfx016@gmail.com>
2024-10-04 10:49:48 +0100

    fix: debug

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -7,85 +7,49 @@ bot(
   pattern: 'install',
   fromMe: true,
   desc: 'Installs External plugins',
-  type: 'user',
+  type: 'system',
  },
- async (message, match) => {
-  if (!match) return await message.sendMessage(message.jid, '_Send a plugin url_');
-
-  try {
-   var url = new URL(match);
-  } catch (e) {
-   console.log(e);
-   return await message.sendMessage(message.jid, '_Invalid Url_');
-  }
-
-  if (url.host === 'gist.github.com') {
-   url.host = 'gist.githubusercontent.com';
-   url = url.toString() + '/raw';
-  } else {
-   url = url.toString();
-  }
-
-  var plugin_name;
-  try {
-   const { data, status } = await axios.get(url);
-   if (status === 200) {
-    var comand = data.match(/(?<=pattern:) ["'](.*?)["']/);
-    plugin_name = comand[0].replace(/["']/g, '').trim().split(' ')[0];
-    if (!plugin_name) {
-     plugin_name = '__' + Math.random().toString(36).substring(8);
-    }
-    fs.writeFileSync(__dirname + '/' + plugin_name + '.js', data);
-    try {
-     require('./' + plugin_name);
-    } catch (e) {
-     fs.unlinkSync(__dirname + '/' + plugin_name + '.js');
-     return await message.sendMessage(message.jid, 'Invalid Plugin\n ```' + e + '```');
-    }
-
-    await installPlugin(url, plugin_name);
-
-    await message.sendMessage(message.jid, `_New plugin installed : ${plugin_name}_`);
-   }
-  } catch (error) {
-   console.error(error);
-   return await message.sendMessage(message.jid, 'Failed to fetch plugin');
-  }
+ async (message, match, m, client) => {
+  if (!match) return message.reply('_Provide Plugin URl_');
+  let url = new URL(match);
+  if (url.host === 'gist.github.com') url = `https://gist.githubusercontent.com${url.pathname}/raw`;
+  const { data } = await axios.get(url);
+  const plugin_name = data.match(/(?<=pattern:) ["'](.*?)["']/)?.[1]?.split(' ')?.[0] || `__${Math.random().toString(36).slice(2)}`;
+  fs.writeFileSync(`${__dirname}/${plugin_name}.js`, data);
+  require(`./${plugin_name}`);
+  await installPlugin(url, plugin_name);
+  message.send(`_Installed: ${plugin_name}_`);
  }
 );
 
-bot({ pattern: 'plugin', fromMe: true, desc: 'plugin list', type: 'user' }, async (message, match) => {
- var mesaj = '';
- var plugins = await PluginDB.findAll();
- if (plugins.length < 1) {
-  return await message.sendMessage(message.jid, '_No external plugins installed_');
- } else {
-  plugins.map((plugin) => {
-   mesaj += '```' + plugin.dataValues.name + '```: ' + plugin.dataValues.url + '\n';
-  });
-  return await message.sendMessage(message.jid, mesaj);
+bot(
+ {
+  pattern: 'plugin',
+  fromMe: true,
+  desc: 'plugin list',
+  type: 'system',
+ },
+ async (message, match, m, client) => {
+  const plugins = await PluginDB.findAll();
+  if (plugins.length < 1) return message.send('_External Plugins Not Found_');
+  message.send(plugins.map((p) => `\`\`\`${p.dataValues.name}\`\`\`: ${p.dataValues.url}`).join('\n'));
  }
-});
+);
 
 bot(
  {
   pattern: 'remove',
   fromMe: true,
   desc: 'Remove external plugins',
-  type: 'user',
+  type: 'system',
  },
- async (message, match) => {
-  if (!match) return await message.sendMessage(message.jid, '_Need a plugin name_');
-
-  var plugin = await PluginDB.findAll({ where: { name: match } });
-
-  if (plugin.length < 1) {
-   return await message.sendMessage(message.jid, '_Plugin not found_');
-  } else {
-   await plugin[0].destroy();
-   delete require.cache[require.resolve('./' + match + '.js')];
-   fs.unlinkSync(__dirname + '/' + match + '.js');
-   await message.sendMessage(message.jid, `Plugin ${match} deleted`);
-  }
+ async (message, match, m, client) => {
+  if (!match) return message.sendMessage('_Need a plugin name_');
+  const plugin = await PluginDB.findOne({ where: { name: match } });
+  if (!plugin) return message.sendMessage('_Plugin not found_');
+  await plugin.destroy();
+  delete require.cache[require.resolve(`./${match}.js`)];
+  fs.unlinkSync(`${__dirname}/${match}.js`);
+  message.sendMessage(`Plugin ${match} deleted`);
  }
 );
