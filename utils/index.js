d1c0fea455e6d91f65b2fcafa9938e8f5a1d97d9
Astro <danielfx016@gmail.com>
2024-10-06 16:54:22 +0100

    fix

--- utils/index.js
+++ utils/index.js
@@ -1,49 +1,37 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
-
-async function loadModulesRecursively(dir, options = {}) {
+function loadModulesRecursively(dir) {
  const exportedModules = {};
- const { excludeDirs = [], excludeFiles = [], fileExtensions = ['.js'], silent = true } = options;
 
- async function traverseDirectory(currentPath) {
-  try {
-   const files = await fs.readdir(currentPath);
+ function traverseDirectory(currentPath) {
+  const files = fs.readdirSync(currentPath);
 
-   for (const file of files) {
-    const filePath = path.join(currentPath, file);
-    const fileStats = await fs.stat(filePath);
+  for (const file of files) {
+   const filePath = path.join(currentPath, file);
+   const fileStats = fs.statSync(filePath);
 
-    if (fileStats.isDirectory() && !excludeDirs.includes(file)) {
-     await traverseDirectory(filePath);
-    } else if (fileStats.isFile() && fileExtensions.includes(path.extname(file)) && !excludeFiles.includes(file)) {
-     const moduleName = path.basename(file, path.extname(file));
-     try {
-      const moduleExports = require(filePath);
-      if (typeof moduleExports === 'object' && moduleExports !== null) {
-       Object.entries(moduleExports).forEach(([key, value]) => {
-        if (exportedModules[key] && !silent) {
-         console.warn(`Warning: Duplicate export '${key}' found in ${file}. Overwriting previous definition.`);
-        }
-        exportedModules[key] = value;
-       });
-      } else {
-       exportedModules[moduleName] = moduleExports;
-      }
-     } catch (error) {
-      if (!silent) {
-       console.error(`Failed to load module ${file}:`, error);
-      }
+   if (fileStats.isDirectory()) {
+    traverseDirectory(filePath);
+   } else if (fileStats.isFile() && path.extname(file) === '.js') {
+    const moduleName = path.basename(file, '.js');
+    try {
+     const moduleExports = require(filePath);
+     if (typeof moduleExports === 'object' && moduleExports !== null) {
+      Object.entries(moduleExports).forEach(([key, value]) => {
+       exportedModules[key] = value;
+      });
+     } else {
+      exportedModules[moduleName] = moduleExports;
      }
+    } catch (error) {
+     console.error(`Failed to load module ${file}:`, error);
     }
-   }
-  } catch (error) {
-   if (!silent) {
-    console.error(`Error reading directory ${currentPath}:`, error);
    }
   }
  }
 
- await traverseDirectory(dir);
+ traverseDirectory(dir);
+
  for (const key of Object.keys(exportedModules)) {
   Object.defineProperty(exportedModules, key, {
    enumerable: false,
@@ -55,4 +43,5 @@ async function loadModulesRecursively(dir, options = {}) {
  return exportedModules;
 }
 
-module.exports = loadModulesRecursively;
+const libPath = path.join(__dirname);
+module.exports = loadModulesRecursively(libPath);
