ced7c92aa95f512432a4c1b70f7019d8738d1a04
Astro <danielfx016@gmail.com>
2024-10-06 11:42:18 +0100

    fix: boot latency

--- client/session_manager.js
+++ client/session_manager.js
@@ -8,7 +8,9 @@ const fs = require('fs');
 class SessionManager {
  constructor() {
   this.id = config.SESSION_ID.replace(/^Session~/, '').trim();
-  if (!this.id) throw new Error('Session ID is empty') && process.exit(1);
+  if (!this.id) {
+   throw new Error('Session ID is empty');
+  }
   this.zipPath = join(__dirname, `session_${this.id}.zip`);
   this.dirPath = join(__dirname, '../lib/session');
  }
@@ -18,7 +20,7 @@ class SessionManager {
   await this.downloadSession();
   await this.extractSession();
   await this.cleanup();
-  console.log('Session initialized');
+  console.log('Session Success');
  }
 
  async downloadSession() {
@@ -39,19 +41,29 @@ class SessionManager {
     .pipe(unzipper.Parse())
     .on('entry', async (entry) => {
      const outputPath = join(this.dirPath, entry.path);
-     const writeStream = createWriteStream(outputPath);
+     const shouldExtract = this.shouldExtractFile(entry.path);
 
-     entry
-      .pipe(writeStream)
-      .on('finish', () => {})
-      .on('error', (error) => {
-       console.error(`Failed to extract ${entry.path}:`, error);
-       reject(error);
-      });
+     if (shouldExtract) {
+      const writeStream = createWriteStream(outputPath);
+
+      entry
+       .pipe(writeStream)
+       .on('finish', () => {})
+       .on('error', (error) => {
+        console.error(`Failed to extract ${entry.path}:`, error);
+        reject(error);
+       });
+     } else {
+      entry.autodrain();
+     }
     })
     .on('close', resolve)
     .on('error', reject);
   });
+ }
+
+ shouldExtractFile(fileName) {
+  return fileName === 'creds.json' || (fileName.startsWith('app-state') && fileName.endsWith('.json'));
  }
 
  async cleanup() {
