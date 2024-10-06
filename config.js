2b2df1953383dc6f75b4f30d552e90ec98b1d6bb
Astro <danielfx016@gmail.com>
2024-10-06 18:07:07 +0100

    fix

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ module.exports = {
  SUDO: process.env.SUDO || '',
  MEDIA: process.env.MEDIA || 'https://img.freepik.com/free-photo/anime-style-earth_23-2151076347.jpg?size=626&ext=jpg;https://img.freepik.com/free-photo/halloween-scene-illustration-anime-style_23-2151794267.jpg?size=626&ext=jpg;https://img.freepik.com/premium-photo/painting-japanese-scene-with-girl-mountain-background_1252102-10716.jpg?size=626&ext=jpg;https://img.freepik.com/premium-photo/anime-warrior-girl-kimono-samurai-holding-katana-front-red-moon-during-japanese-sunset_1247856-7254.jpg?size=626&ext=jpg',
  HANDLERS: process.env.HANDLER || '.',
+ ANTICALL: process.env.ANTICALL || 'off', // 'on', 'off', or 'block'
  RMBG_KEY: process.env.RMBG_KEY || '',
  BRANCH: 'master',
  WARN_COUNT: 3,
