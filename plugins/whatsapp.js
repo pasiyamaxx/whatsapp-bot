efbcf6891c1ad5811ac282b3a90222c04b676a40
Astro <danielfx016@gmail.com>
2024-10-06 14:39:35 +0100

    fix

--- plugins/whatsapp.js
+++ plugins/whatsapp.js
@@ -142,9 +142,8 @@ bot(
   type: 'whatsapp',
  },
  async (message, match, m, client) => {
-  if (!message.reply_message?.image && !message.reply_message.video && !message.reply_message.audio) return await message.reply('_Reply Status_');
-  let quotedMsg = m.quoted;
-  return await message.copyNForward(message.user, m.quoted, { quoted: quotedMsg });
+  if (!message.reply_message?.image && !message.reply_message.video && !message.reply_message.audio) return await message.reply('_Reply to a status message containing an image, video, or audio_');
+  return await message.copyNForward(message.user, message.reply_message.message, { quoted: message.data });
  }
 );
 
