7b2ddcbce6705f47de7f10d5dfd18cf270abf889
Astro <danielfx016@gmail.com>
2024-10-07 18:49:50 +0100

    fix

--- plugins/search.js
+++ plugins/search.js
@@ -1,4 +1,4 @@
-const { bot, getJson, lyrics, sleep, Google, getFloor, onwhatsapp } = require('../lib');
+const { bot, getJson, Google, getFloor, onwhatsapp } = require('../utils');
 const moment = require('moment');
 const axios = require('axios');
 
@@ -12,10 +12,9 @@ bot(
  async (message) => {
   const apiUrl = 'https://api.polygon.io/v2/reference/news?apiKey=Y4iTYoJANwppB8I3Bm4QVWdV5oXlvc45';
   const data = await getJson(apiUrl);
-  if (!data.results || data.results.length === 0) return message.send('*No forex news available at the moment.*');
+  if (!data.results || data.results.length === 0) return message.reply('*No forex news available at the moment.*');
   const output = data.results.map((article, index) => `*Title:* ${article.title}\n` + `*Publisher:* ${article.publisher.name}\n` + `*Published UTC:* ${article.published_utc}\n` + `*Article URL:* ${article.article_url}\n` + (index < data.results.length - 1 ? '---\n\n' : '')).join('');
-
-  return message.send(output, { quoted: message.data });
+  return message.reply(output);
  }
 );
 
@@ -30,28 +29,11 @@ bot(
   const apiUrl = 'https://api.polygon.io/v1/marketstatus/now?apiKey=Y4iTYoJANwppB8I3Bm4QVWdV5oXlvc45';
   const data = await getJson(apiUrl);
 
-  if (!data) return message.send('*Failed to fetch forex market status.*');
+  if (!data) return message.reply('*Failed to fetch forex market status.*');
 
   const output = `*Forex Market Status:*\n` + `After Hours: ${data.afterHours ? 'Closed' : 'Open'}\n` + `Market: ${data.market ? 'Open' : 'Closed'}\n\n` + `*Currencies:*\n` + `Crypto: ${data.currencies.crypto}\n` + `FX: ${data.currencies.fx}\n\n` + `*Exchanges:*\n` + `NASDAQ: ${data.exchanges.nasdaq}\n` + `NYSE: ${data.exchanges.nyse}\n` + `OTC: ${data.exchanges.otc}\n\n` + `*Indices Groups:*\n` + `S&P: ${data.indicesGroups.s_and_p}\n` + `Societe Generale: ${data.indicesGroups.societe_generale}\n` + `MSCI: ${data.indicesGroups.msci}\n` + `FTSE Russell: ${data.indicesGroups.ftse_russell}\n` + `MStar: ${data.indicesGroups.mstar}\n` + `MStarC: ${data.indicesGroups.mstarc}\n` + `CCCY: ${data.indicesGroups.cccy}\n` + `CGI: ${data.indicesGroups.cgi}\n` + `NASDAQ: ${data.indicesGroups.nasdaq}\n` + `Dow Jones: ${data.indicesGroups.dow_jones}\n\n` + `*Server Time:* ${data.serverTime}`;
 
-  return message.send(output, { quoted: message });
- }
-);
-
-bot(
- {
-  pattern: 'fxpairs',
-  fromMe: false,
-  desc: 'Fetches a list of active forex currency pairs',
-  type: 'search',
- },
- async (message) => {
-  const apiUrl = 'https://api.polygon.io/v3/reference/tickers?market=fx&active=true&apiKey=Y4iTYoJANwppB8I3Bm4QVWdV5oXlvc45';
-  const data = await getJson(apiUrl);
-  if (!data || !data.results || data.results.length === 0) return message.send('*Failed to fetch forex currency pairs.*');
-  const output = data.results.map((pair) => `${pair.ticker}: ${pair.name}`).join('\n');
-
-  return message.send(`*Active Forex Currency Pairs:*\n\n${output}`, { quoted: message });
+  return message.reply(output);
  }
 );
 
@@ -67,29 +49,12 @@ bot(
   const apiUrl = `https://api.exchangerate-api.com/v4/latest/${currencyCode}`;
   const data = await getJson(apiUrl);
 
-  if (!data || !data.rates) return message.send(`*Failed to fetch exchange rates for ${currencyCode}.*`);
+  if (!data || !data.rates) return message.reply(`*Failed to fetch exchange rates for ${currencyCode}.*`);
   const output = Object.entries(data.rates)
    .map(([currency, rate]) => `${currency}: ${rate.toFixed(4)}`)
    .join('\n');
 
-  return message.send(`*Foreign Exchange Rates (${data.base})*\n\n${output}`, { quoted: message });
- }
-);
-
-bot(
- {
-  pattern: 'stocks',
-  fromMe: false,
-  desc: 'Fetches a list of active stock tickers',
-  type: 'search',
- },
- async (message, match) => {
-  const limit = match || 100;
-  const apiUrl = `https://api.polygon.io/v3/reference/tickers?active=true&limit=${limit}&apiKey=Y4iTYoJANwppB8I3Bm4QVWdV5oXlvc45`;
-  const data = await getJson(apiUrl);
-  if (!data || !data.results || data.results.length === 0) return message.send('*No active stock tickers found.*');
-  const output = data.results.map((ticker) => `${ticker.ticker}: ${ticker.name}`).join('\n');
-  return message.send(`*Active Stock Tickers (Limit: ${limit}):*\n\n${output}`, { quoted: message });
+  return message.reply(`*Foreign Exchange Rates (${data.base})*\n\n${output}`);
  }
 );
 
@@ -101,29 +66,12 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.send('*Example : weather delhi*');
+  if (!match) return await message.reply('*Example : weather delhi*');
   const data = await getJson(`http://api.openweathermap.org/data/2.5/weather?q=${match}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en`).catch(() => {});
-  if (!data) return await message.send(`_${match} not found_`);
+  if (!data) return await message.reply(`_${match} not found_`);
   const { name, timezone, sys, main, weather, visibility, wind } = data;
   const degree = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'][getFloor(wind.deg / 22.5 + 0.5) % 16];
-  return await message.send(`*Name :* ${name}\n*Country :* ${sys.country}\n*Weather :* ${weather[0].description}\n*Temp :* ${getFloor(main.temp)}°\n*Feels Like :* ${getFloor(main.feels_like)}°\n*Humidity :* ${main.humidity}%\n*Visibility  :* ${visibility}m\n*Wind* : ${wind.speed}m/s ${degree}\n*Sunrise :* ${moment.utc(sys.sunrise, 'X').add(timezone, 'seconds').format('hh:mm a')}\n*Sunset :* ${moment.utc(sys.sunset, 'X').add(timezone, 'seconds').format('hh:mm a')}`);
- }
-);
-
-bot(
- {
-  pattern: 'lyrics ?(.*)',
-  fromMe: false,
-  desc: 'Search lyrics of Song',
-  type: 'search',
- },
- async (message, match) => {
-  if (!match) return await message.sendReply(`\`\`\`Wrong format\n\n${message.prefix}lyrics Just the two of Us\`\`\``);
-  const msg = await message.reply("_Searching for '" + match + "'_");
-  const songLyrics = await lyrics(match);
-  await msg.edit('_Lyrics Found!_');
-  await sleep(1500);
-  return await msg.edit(songLyrics);
+  return await message.reply(`*Name :* ${name}\n*Country :* ${sys.country}\n*Weather :* ${weather[0].description}\n*Temp :* ${getFloor(main.temp)}°\n*Feels Like :* ${getFloor(main.feels_like)}°\n*Humidity :* ${main.humidity}%\n*Visibility  :* ${visibility}m\n*Wind* : ${wind.speed}m/s ${degree}\n*Sunrise :* ${moment.utc(sys.sunrise, 'X').add(timezone, 'seconds').format('hh:mm a')}\n*Sunset :* ${moment.utc(sys.sunset, 'X').add(timezone, 'seconds').format('hh:mm a')}`);
  }
 );
 
@@ -135,7 +83,7 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.sendReply('_Provide Me A Query' + message.pushName + '_\n\n' + message.prefix + 'google fxop-md');
+  if (!match) return await message.reply('_Provide Me A Query' + message.pushName + '_\n\n' + message.prefix + 'google fxop-md');
   const msg = await message.reply('_Searching for ' + match + '_');
   const res = await Google(match);
   return await msg.edit(res);
@@ -150,11 +98,11 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.send('*Please provide a phone number.*');
+  if (!match) return await message.reply('*Please provide a phone number.*');
 
   const phoneNumber = match.trim();
   const result = await onwhatsapp(phoneNumber);
-  return await message.send(result, { quoted: message });
+  return await message.reply(result);
  }
 );
 
@@ -171,9 +119,9 @@ bot(
   const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${query}`;
   const response = await getJson(url);
   if (response.type === 'standard') {
-   await message.send(`*Wikipedia result:*\n\nTitle: ${response.title}\n\n${response.extract}\n\nRead more: ${response.content_urls.desktop.page}`);
+   await message.reply(`*Wikipedia result:*\n\nTitle: ${response.title}\n\n${response.extract}\n\nRead more: ${response.content_urls.desktop.page}`);
   } else {
-   await message.send('No Wikipedia article found for your query.');
+   await message.reply('No Wikipedia article found for your query.');
   }
  }
 );
@@ -186,15 +134,15 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.sendReply('Please provide a movie title.');
+  if (!match) return await message.reply('Please provide a movie title.');
   const query = encodeURIComponent(match);
   const url = `http://www.omdbapi.com/?t=${query}&apikey=4fc4cf8c`;
   const response = await axios.get(url);
   const movie = response.data;
   if (movie.Response === 'True') {
-   await message.send(`*Title:* ${movie.Title}\n*Year:* ${movie.Year}\n*Genre:* ${movie.Genre}\n*Plot:* ${movie.Plot}\n*IMDB Rating:* ${movie.imdbRating}\n\n*More Info:* ${movie.Poster}`);
+   await message.reply(`*Title:* ${movie.Title}\n*Year:* ${movie.Year}\n*Genre:* ${movie.Genre}\n*Plot:* ${movie.Plot}\n*IMDB Rating:* ${movie.imdbRating}\n\n*More Info:* ${movie.Poster}`);
   } else {
-   await message.send('Movie not found.');
+   await message.reply('Movie not found.');
   }
  }
 );
@@ -207,16 +155,16 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.sendReply('Please provide a word to define.');
+  if (!match) return await message.reply('Please provide a word to define.');
 
   const query = encodeURIComponent(match);
   const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
   const response = await axios.get(url);
   const definition = response.data[0];
   if (definition) {
-   await message.send(`*Word:* ${definition.word}\n*Meaning:* ${definition.meanings[0].definitions[0].definition}\n*Example:* ${definition.meanings[0].definitions[0].example || 'N/A'}`);
+   await message.reply(`*Word:* ${definition.word}\n*Meaning:* ${definition.meanings[0].definitions[0].definition}\n*Example:* ${definition.meanings[0].definitions[0].example || 'N/A'}`);
   } else {
-   await message.send('Definition not found.');
+   await message.reply('Definition not found.');
   }
  }
 );
@@ -229,16 +177,16 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.sendReply('Please provide a topic for news.');
+  if (!match) return await message.reply('Please provide a topic for news.');
 
   const query = encodeURIComponent(match);
   const url = `https://newsapi.org/v2/everything?q=${query}&apiKey=6798c308cb454b4cbba9af98ee488507&pageSize=1`;
   const response = await axios.get(url);
   const news = response.data.articles[0];
   if (news) {
-   await message.send(`*Headline:* ${news.title}\n*Source:* ${news.source.name}\n*Published at:* ${news.publishedAt}\n\n${news.description}\n\nRead more: ${news.url}`);
+   await message.reply(`*Headline:* ${news.title}\n*Source:* ${news.source.name}\n*Published at:* ${news.publishedAt}\n\n${news.description}\n\nRead more: ${news.url}`);
   } else {
-   await message.send('No news found for your query.');
+   await message.reply('No news found for your query.');
   }
  }
 );
@@ -251,15 +199,15 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.sendReply('_Please provide a song title or artist._');
+  if (!match) return await message.reply('_Please provide a song title or artist._');
 
   const query = encodeURIComponent(match);
   const url = `https://api.lyrics.ovh/v1/${query}`;
   const response = await axios.get(url);
   if (response.data.lyrics) {
-   await message.send(`*Lyrics:*\n\n${response.data.lyrics}`);
+   await message.reply(`*Lyrics:*\n\n${response.data.lyrics}`);
   } else {
-   await message.send('Lyrics not found.');
+   await message.reply('Lyrics not found.');
   }
  }
 );
@@ -272,15 +220,15 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.sendReply('Please provide a cryptocurrency symbol (e.g., bitcoin, ethereum).');
+  if (!match) return await message.reply('Please provide a cryptocurrency symbol (e.g., bitcoin, ethereum).');
   const query = encodeURIComponent(match.toLowerCase());
   const url = `https://api.coingecko.com/api/v3/simple/price?ids=${query}&vs_currencies=usd`;
   const response = await axios.get(url);
   const priceData = response.data;
   if (priceData[query]) {
-   await message.send(`*${match.toUpperCase()} Price:* $${priceData[query].usd}`);
+   await message.reply(`*${match.toUpperCase()} Price:* $${priceData[query].usd}`);
   } else {
-   await message.send('Cryptocurrency not found.');
+   await message.reply('Cryptocurrency not found.');
   }
  }
 );
@@ -293,16 +241,16 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.sendReply('Please provide an anime title.');
+  if (!match) return await message.reply('Please provide an anime title.');
 
   const query = encodeURIComponent(match);
   const url = `https://api.jikan.moe/v4/anime?q=${query}&limit=1`;
   const response = await axios.get(url);
   const anime = response.data.data[0];
   if (anime) {
-   await message.send(`*Title:* ${anime.title}\n*Episodes:* ${anime.episodes}\n*Status:* ${anime.status}\n*Score:* ${anime.score}\n*Synopsis:* ${anime.synopsis}\n\n*More Info:* ${anime.url}`);
+   await message.reply(`*Title:* ${anime.title}\n*Episodes:* ${anime.episodes}\n*Status:* ${anime.status}\n*Score:* ${anime.score}\n*Synopsis:* ${anime.synopsis}\n\n*More Info:* ${anime.url}`);
   } else {
-   await message.send('Anime not found.');
+   await message.reply('Anime not found.');
   }
  }
 );
@@ -315,54 +263,16 @@ bot(
   type: 'search',
  },
  async (message, match) => {
-  if (!match) return await message.sendReply('Please provide a GitHub username.');
+  if (!match) return await message.reply('Please provide a GitHub username.');
 
   const query = encodeURIComponent(match);
   const url = `https://api.github.com/users/${query}`;
   const response = await axios.get(url);
   const user = response.data;
   if (user) {
-   await message.send(`*Name:* ${user.name || 'N/A'}\n*Username:* ${user.login}\n*Bio:* ${user.bio || 'N/A'}\n*Public Repos:* ${user.public_repos}\n*Followers:* ${user.followers}\n\n*Profile URL:* ${user.html_url}`);
+   await message.reply(`*Name:* ${user.name || 'N/A'}\n*Username:* ${user.login}\n*Bio:* ${user.bio || 'N/A'}\n*Public Repos:* ${user.public_repos}\n*Followers:* ${user.followers}\n\n*Profile URL:* ${user.html_url}`);
   } else {
-   await message.send('GitHub user not found.');
-  }
- }
-);
-
-bot(
- {
-  pattern: 'cat',
-  fromMe: false,
-  desc: 'Get a random cat image',
-  type: 'search',
- },
- async (message) => {
-  const url = `https://api.thecatapi.com/v1/images/search`;
-  const response = await axios.get(url);
-  const cat = response.data[0];
-  if (cat) {
-   await message.send(cat.url, 'image', "Here's a random cat for you!");
-  } else {
-   await message.sendMessage('Could not fetch a cat image.');
-  }
- }
-);
-
-bot(
- {
-  pattern: 'dog',
-  fromMe: false,
-  desc: 'Get a random dog image',
-  type: 'search',
- },
- async (message) => {
-  const url = `https://dog.ceo/api/breeds/image/random`;
-  const response = await axios.get(url);
-  const dog = response.data.message;
-  if (dog) {
-   await message.sendFileFromUrl(dog, 'image', "Here's a random dog for you!");
-  } else {
-   await message.sendMessage('Could not fetch a dog image.');
+   await message.reply('GitHub user not found.');
   }
  }
 );
