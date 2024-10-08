ff190b309f411c1c764f5512b5f8b5459e8c6035
Astro <danielfx016@gmail.com>
2024-10-08 17:42:03 +0100

    fix

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const path = require('path');
 const config = require('./config');
-const { connect } = require('./lib');
+const { delay } = require('baileys');
+const { connect, getandRequirePlugins } = require('./lib');
 const { requireJS } = require('./utils');
-const { getandRequirePlugins } = require('./lib/database').Plugins;
 const { SessionManager } = require('./client');
 const app = express();
 const PORT = process.env.PORT || '8000';
 async function makeSession() {
-const session = new SessionManager();
-session.createSession();
+ const session = new SessionManager();
+ await session.createSession();
 }
 async function initialize() {
-await requireJS(path.join(__dirname, '/lib/database/'));
-await config.DATABASE.sync();
-await requireJS(path.join(__dirname, '/plugins/'));
-await getandRequirePlugins();
-return await connect();
+ await requireJS(path.join(__dirname, '/lib/database/'));
+ await config.DATABASE.sync();
+ await requireJS(path.join(__dirname, '/plugins/'));
+ await getandRequirePlugins();
+ return await connect();
 }
 
 app.listen(PORT, async () => {
-await makeSession();
-return await initialize();
+ await makeSession();
+ await delay(5000);
+ return await initialize();
 });
