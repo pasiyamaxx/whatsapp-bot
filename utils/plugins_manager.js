c847da4dc2b9d20da30ad4197907a62a40671534
Astro <danielfx016@gmail.com>
2024-10-08 19:36:41 +0100

    fix

--- utils/plugins_manager.js
+++ utils/plugins_manager.js
@@ -1,32 +1,18 @@
 const config = require('../config');
-
 var commands = [];
 
-/**
- * Registers a command with its associated function and modifies command patterns.
- * @param {Object} cmdInfo - Information about the command.
- * @param {String} cmdInfo.pattern - The command pattern to match.
- * @param {Boolean} [cmdInfo.fromMe=false] - Indicates if the command is from the bot itself.
- * @param {Boolean} [cmdInfo.dontAddCommandList=false] - If true, the command won't be added to the command list.
- * @param {String} [cmdInfo.type='misc'] - The type of command.
- * @param {Function} func - The function to execute when the command is called.
- * @returns {Object} - The modified commandInfo object.
- */
 function bot(cmdInfo, func) {
  cmdInfo.function = func;
- if (cmdInfo.pattern) {
-  cmdInfo.originalPattern = cmdInfo.pattern; // Store the original pattern
-  cmdInfo.pattern = new RegExp(`^(${config.HANDLERS})\\s*(${cmdInfo.pattern})(?:\\s+(.*))?$`, 'i'); // Create a new RegExp
- }
- cmdInfo.dontAddCommandList = cmdInfo.dontAddCommandList || false; // Default to false
- cmdInfo.fromMe = cmdInfo.fromMe || false; // Default to false
- cmdInfo.type = cmdInfo.type || 'misc'; // Default to 'misc'
-
+ cmdInfo.pattern = new RegExp(`^(${config.HANDLERS})\\s*(${cmdInfo.pattern})(?:\\s+(.*))?$`, 'i');
+ cmdInfo.dontAddCommandList = cmdInfo.dontAddCommandList || false;
+ cmdInfo.fromMe = cmdInfo.fromMe || false;
+ cmdInfo.isGroup = cmdInfo.isGroup || false;
+ cmdInfo.type = cmdInfo.type || 'misc';
  commands.push(cmdInfo);
  return cmdInfo;
 }
 
-module.exports={
-  bot,
-  commands
-}
+module.exports = {
+ bot,
+ commands,
+};
